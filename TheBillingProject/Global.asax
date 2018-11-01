19123fcb687089abbbc8fabc9d49406f9a9262c6
Jose Abimael Martinez <jamartinez@atlantica.do>
2018-11-01 14:58:29 -0400

    first commit

---
+++ TheBillingProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TheBillingProject.MvcApplication" Language="C#" %>
---
+++ TheBillingProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TheBillingProject.Startup))]
+namespace TheBillingProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
