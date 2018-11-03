4d6b6cfea0f17890f16181b2b0dbacb6066a00a6
fmedinar <fmedinar07@hotmail.com>
2018-11-03 00:32:43 -0400

    Apply template to article view

--- TheBillingProject/Models/Article.cs
+++ TheBillingProject/Models/Article.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace TheBillingProject.Models
 {
     public class Article
-    { 
+    {
         //
+        [Display(Name = "Id")]
         public string _id { get; set; }
+        [Display(Name = "Description")]
         public string description { get; set; }
+        [Display(Name = "Status")]
         public string status { get; set; }
+        [Display(Name = "Unit Price")]
         public double unit_price { get; set; }
     }
 }
