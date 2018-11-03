d4fb175baf1d3b929185c3c3db97a1d0e052658f
fmedinar <fmedinar07@hotmail.com>
2018-11-02 22:16:30 -0400

    Carga template

--- TheBillingProject/App_Start/BundleConfig.cs
+++ TheBillingProject/App_Start/BundleConfig.cs
@@ -9,22 +9,24 @@ namespace TheBillingProject
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                      "~/assets/libs/jquery/dist/jquery.min.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
+                      "~/Scripts/jquery.validate*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
+                      "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
+                      "~/assets/libs/popper.js/dist/umd/popper.min.js",
+                      "~/assets/libs/bootstrap/dist/js/bootstrap.min.js",
+                      "~/assets/libs/perfect-scrollbar/dist/perfect-scrollbar.jquery.min.js",
+                      "~/assets/extra-libs/sparkline/sparkline.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                      "~/dist/css/style.min.css"));
         }
     }
 }
